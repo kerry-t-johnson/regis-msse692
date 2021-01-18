2aafc54fb7677a62644d0f0f15556bd1f6d3d92f
Kerry Johnson <kerry.t.johnson@gmail.com>
2021-01-18 07:41:40 -0500

    Give PHP access to MySQL

--- assignment-1/html/index.php
+++ assignment-1/html/index.php
@@ -3,7 +3,17 @@
   <title>MSSE692 Test</title>
  </head>
  <body>
- <?php echo '<p>Hello MSSE692</p>'; ?> 
- <?php echo phpinfo(); ?>
+ <?php
+    echo '<p>Hello MSSE692</p>';
+   
+    $myPDO = new PDO('mysql:host=db;dbname=msse692',
+	    /* username = */ 'msse692',
+	    /* password = */ 'P@55w0rd');
+
+    echo 'MySQL Version: ' . $myPDO->query('select version()')->fetchColumn();
+
+    echo phpinfo();
+
+    ?>
  </body>
 </html>
